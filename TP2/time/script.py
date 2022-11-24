3088cbc136745e2c094daabe08d0af9c61de17e6
srlucasromulo <lucas.romulo.sr@gmail.com>
2022-11-24 01:37:00 -0300

    time script

--- TP2/time/script.py
+++ TP2/time/script.py
@@ -4,4 +4,4 @@ df = pd.read_csv('time_elapsed', names=['function', 'time'], sep=';')
 
 df = df.groupby(['function'], as_index=False).mean()
 
-df.to_csv('time_results', index=False, sep=';')
+df.to_csv('../profile', index=False, sep=';')
